240cc728eaf6515a301dbec0c55a77a68f7b4263
senthil <senthil.santhoshgopal@gmail.com>
2025-07-19 21:28:03 +1200

    Add graphql api

---
+++ Api.GraphQL/Types/Query.cs
@@ -0,0 +1,23 @@
+﻿using Application.Articles.GetAllArticles;
+using MediatR;
+
+namespace Api.GraphQL.Types
+{
+    public class Query
+    {
+        private readonly IMediator _mediator;
+
+        public Query(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [GraphQLDescription("Get all articles")]
+        public async Task<IEnumerable<ArticleDto>> GetAllArticlesAsync()
+        {
+            var query = new GetAllArticlesQuery();
+            var articles = await _mediator.Send(query);
+            return articles;
+        }
+    }
+}
